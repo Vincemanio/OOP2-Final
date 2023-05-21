85c0d2888c82baf1020296376e1da37955259ec4
Vincemanio <119021720+Vincemanio@users.noreply.github.com>
2023-05-21 01:10:42 -0600

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using project.Data;
+using project.Services;
+
+namespace project;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"ProductDB.db3");
+		builder.Services.AddSingleton<OrderService>(p=> ActivatorUtilities.CreateInstance<OrderService>(p,dbPath));
+
+		return builder.Build();
+	}
+}
